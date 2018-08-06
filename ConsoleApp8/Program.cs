d02b277e4cb0c583ca8b19818ea8aa0997c9221b
George Doggo <volty0@orange.fr>
2018-08-06 16:34:46 +0200

    tata

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -10,6 +10,56 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)
         {
+            int[] scores = new int[] { 97, 92, 81, 60 };
+            /* requete1(scores);
+             requete2(scores);*/
+
+            Console.WriteLine("De Quel mot voulez vous savoir la composition?:");
+            string valeur = "";
+            valeur = Console.ReadLine();
+            requete3(valeur);
+
         }
+
+        static public void requete1(int[] scores)
+        {
+            var requete = from score in scores
+                          where score > 80
+                          select score;
+            foreach (var resultat in requete)
+            {
+                Console.WriteLine(resultat);
+            }
+        }
+
+        static public void requete2(int[] scores)
+        {
+            var requete = from score in scores
+                          orderby score ascending
+                          select score;
+            foreach (var resultat in requete)
+            {
+                Console.WriteLine(resultat);
+            }
+        }
+
+        static public void requete3(string chaine)
+        {
+
+            var requete = from letter in chaine
+                          group letter by letter into groupe
+                          select new
+                          {
+                              Lettre = groupe.Key,
+                              Nombre = groupe.Count() 
+                          };
+            foreach (var resultat in requete)
+            {
+                Console.WriteLine(resultat.Lettre+" : "+resultat.Nombre);
+            }
+
+        }
+
+
     }
 }
